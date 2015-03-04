4be65e3ad5253f9674b94c9a676f491c7c7d7ae8
Will Gray Roncal <wgr@jhu.edu>
2015-03-03 22:02:42 -0500

    fixing i2g setup
    Former-commit-id: 3737a23999fccad01bf9843e493a88cae20700b5

--- i2g_setup.m
+++ i2g_setup.m
@@ -1,9 +1,10 @@
 % Setup script for I2G "toolbox"
 % This should be run during each matlab session 
 
+cd('/mnt/pipeline/tools/CAJAL3d')
 addpath(pwd) %top level directory
 addpath('./data')
-addpath(genpath('packages'))
+addpath(genpath('./packages'))
 addpath('test')
 addpath(genpath('external/tools'))
 
