862bd0f2caafe6f7d9f0b4b53a5b6b6e99f668ce
Will Gray Roncal <wgr@jhu.edu>
2015-03-30 13:41:28 -0700

    Major code commit - gala and block merge
    Former-commit-id: 15fcd99c7533825a3714ea72cda97c353adcfdc5 [formerly 963f23d649d65e418af4344c84288fbc6019da19]
    Former-commit-id: 010f5f0d3f895f7628f36adde5394f560b35e8d4

--- packages/graph_gen/build_graph_volume_deploy.py
+++ packages/graph_gen/build_graph_volume_deploy.py
@@ -8,7 +8,7 @@
 ##
 ## Make sure that you specify the environment variable MATLAB_EXE_LOCATION inside the LONI module.  This can be
 ## set under advanced options on the 'Execution' tab in the module set up.
-############################################################################################
+
 # (c) [2014] The Johns Hopkins University / Applied Physics Laboratory All Rights Reserved. Contact the JHU/APL Office of Technology Transfer for any additional rights.  www.jhuapl.edu/ott
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,8 +23,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-############################################################################################
-
 from sys import argv
 from sys import exit
 import sys
@@ -36,61 +34,43 @@ from subprocess import Popen,PIPE
 params = list(argv)
 del params[0]
 
+# edgeToken, edgeServer, nodeToken, nodeServer
 
-imgToken = params[0:2]
-imgServer = params[2:4]
-annoToken = params[2:4]
+evalToken = params[0:2]
+synTruthVol = params[2:4]
 queryFile = params[4:6]
-useSemaphore = params[6:8]
-serviceLocation = params[8:10]
-dilXY = params[10:12]
-dilZ = params[12:14]
-thresh = params[14:16]
-emCube = params[16:18]
-tokenFile = params[18:20]
-labelOut = params[20:22]
+resolution = params[6:8]
+useSemaphore = params[8:10]
+attEdge = params[10:12]
+edgeList = params[12:14]
+graphName = params[14:16]
 
-print labelOut
+evalParam = evalToken[1].split(',')
+edgeToken = ['-s', evalParam[0]]
+edgeServer = ['-s', evalParam[1]]
+nodeToken = ['-s', evalParam[2]]
+nodeServer = ['-s', evalParam[3]]
+
+print edgeToken
+print edgeServer 
+print nodeToken
+print nodeServer
+
 # get root directory of framework
-frameworkRoot = os.getenv("CAJAL3D_LOCATION")
+frameworkRoot = os.getenv("I2G_LOCATION")
 if frameworkRoot is None:
-    raise Exception('You must set the CAJAL3D_LOCATION environment variable so the wrapper knows where the framework is!')
+    raise Exception('You must set the I2G_LOCATION environment variable so the wrapper knows where the framework is!')
 
 # Gen path of matlab wrapper
-wrapper = os.path.join(frameworkRoot, 'api', 'matlab','wrapper','basicWrapper.py')
+wrapper = os.path.join(frameworkRoot, 'packages', 'utilities','basicWrapperI2G.py')
 
-# Build call to EM Cube Cutout
-args = [wrapper] + ["packages/cubeCutout/cubeCutout.m"] + imgToken + queryFile + emCube + useSemaphore + ["-d", "0"] + serviceLocation + ["-b", "0"]
-print args
-# Call Cube Cutout
-process = Popen(args, stdout=PIPE, stderr=PIPE)
-output = process.communicate()
-proc_error = output[1]
-proc_output = output[0]
-exit_code = process.wait()
+# Build call to Graph Gen
 
-# Write std out stream
-print "#######################\n"
-print "Output From EM Cube Cutout\n"
-print "#######################\n\n\n"
-print proc_output
+args = [wrapper] + ["packages/graph_gen/build_graph_volume.m"] + nodeToken + nodeServer + edgeToken + edgeServer + synTruthVol + queryFile + resolution + useSemaphore + attEdge + edgeList + graphName
 
-# If exit code != 0 exit
-if exit_code != 0:
-    # it bombed.  Write out matlab errors and return error code
-    sys.stderr.write("Error from Cube Cutout:\n\n")
-    sys.stderr.write(proc_error)
-    exit(exit_code)
-
-
-#emCube,dbToken, dilXY, dilZ, thresh,useSemaphore, errorPageLocation, serviceLocation, varargin
-
-# Build call to Segment Watershed
-args = [wrapper] + ["packages/segmentWatershed/segmentWatershedWrapper.m"] + emCube + annoToken + dilXY + dilZ +  thresh + useSemaphore + ["-s", "/mnt/pipeline/errorPages"] + serviceLocation + labelOut + tokenFile #+ ["-b", "0"]
-print 'made it'
 print args
 
-# Call Segment Watershed Detector
+# Call Graph Gen
 process = Popen(args, stdout=PIPE, stderr=PIPE)
 output = process.communicate()
 proc_error2 = output[1]
@@ -99,14 +79,14 @@ exit_code2 = process.wait()
 
 # Write std out stream
 print "########################################\n"
-print "Output From Membrane Detector\n"
+print "Output From Build Graph\n"
 print "########################################\n\n\n"
 print proc_output2
 
 # If exit code != 0 exit
 if exit_code2 != 0:
     # it bombed.  Write out matlab errors and return error code
-    sys.stderr.write("Error from Segment Watershd:\n\n")
+    sys.stderr.write("Error from Graph Gen:\n\n")
     sys.stderr.write(proc_error2)
     exit(exit_code2)
 
